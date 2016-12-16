cb3260a054bddd3cea9d1edc324a484055d4682e
pawar-amol <amol.pawar@globant.com>
2016-12-16 11:31:24 +0530

    Added trend in product module

--- WebAPI-DI/Models/Product.cs
+++ WebAPI-DI/Models/Product.cs
@@ -11,5 +11,6 @@ namespace ProductStore.Models
         public string Name { get; set; }
         public decimal Price { get; set; }
         public string Type { get; set; }
+        public string Trend { get; set; }
     }
 }
